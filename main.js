d56d26bd37e984a6eb46dc25d0910152c69d5841
MitchHickox <mhickox@rpionline.com>
2019-07-29 13:13:22 -0400

    try again

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
     // Prevent Chrome 67 and earlier from automatically showing the prompt
     e.preventDefault();
     // Stash the event so it can be triggered later.
-    deferredPrompt = e;
+    let deferredPrompt = e;
     console.log("before the install prompt");
 
 
