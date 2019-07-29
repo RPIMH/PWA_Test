b3490fc1e98dbf68470840561730578194b52b0b
MitchHickox <mhickox@rpionline.com>
2019-07-29 13:09:23 -0400

    more

--- main.js
+++ main.js
@@ -1,11 +1,43 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+        navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function(err) {
+        }, function(err) {
         // registration failed :(
         console.log('ServiceWorker registration failed: ', err);
-      });
+        });
     });
-  }
+}
+
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    console.log("before the install prompt");
+
+
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+        console.log('display-mode is standalone');
+      }
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice
+        .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+        } else {
+            console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+        });
+});
+
+
+
+window.addEventListener('appinstalled', (evt) => {
+    console.log('a2hs installed');
+  });
--- serviceworker.js
+++ serviceworker.js
@@ -6,11 +6,6 @@ var urlsToCache = [
 ];
 
 
-
-
-
-
-
 self.addEventListener('install', function(event) {
     // Perform install steps
     console.log("in the install");
@@ -36,18 +31,63 @@ self.addEventListener('activate', function(event){
 
 
 
-  self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function(event) {
     event.respondWith(
-      caches.match(event.request)
+        caches.match(event.request)
         .then(function(response) {
-          // Cache hit - return response
-          if (response) {
+            // Cache hit - return response
+            if (response) {
             console.log("hit the cache");
             return response;
-          }
-          console.log("did not hit the cache this time");
-          return fetch(event.request);
+            }
+            console.log("did not hit the cache this time");
+            return fetch(event.request);
         }
-      )
+        )
+    );
+});
+
+
+
+
+
+
+self.addEventListener('push', function(event) {
+    console.log('Received a push message', event);
+  
+    var title = 'Yay a message.';
+    var body = 'We have received a push message.';
+    var icon = 'DN_512x512.png';
+    var tag = 'simple-push-demo-notification-tag';
+  
+    event.waitUntil(
+      self.registration.showNotification(title, {
+        body: body,
+        icon: icon,
+        tag: tag
+      })
     );
   });
+
+self.addEventListener('notificationclick', function(event) {
+    console.log('On notification click: ', event.notification.tag);
+    // Android doesn’t close the notification when you click on it
+    // See: http://crbug.com/463146
+    event.notification.close();
+
+    // This looks to see if the current is already open and
+    // focuses if it is
+    event.waitUntil(clients.matchAll({
+        type: 'window'
+    }).then(function(clientList) {
+        for (var i = 0; i < clientList.length; i++) {
+        var client = clientList[i];
+        if (client.url === '/' && 'focus' in client) {
+            return client.focus();
+        }
+        }
+        if (clients.openWindow) {
+        return clients.openWindow('/');
+        }
+    }));
+});
