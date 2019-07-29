b1108d8294553d945036f4240db0fd3225e7bcca
MitchHickox <mhickox@rpionline.com>
2019-07-29 15:24:09 -0400

    alert message

--- serviceworker.js
+++ serviceworker.js
@@ -72,6 +72,7 @@ self.addEventListener('push', function(event) {
 self.addEventListener('notificationclick', function(event) {
     console.log('On notification click: ', event.notification.tag);
     console.log(event);
+    alert("You said " + event.action) + "!");
     // Android doesn’t close the notification when you click on it
     // See: http://crbug.com/463146
     event.notification.close();
